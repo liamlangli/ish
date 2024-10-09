396c09ef4c22209ea456cf2bb246968398d4e700
Liam <lilang8936@gmail.com>
2024-10-09 11:44:08 +0800

    change repo

--- .profile
+++
@@ -1,7 +0,0 @@
-export PS1='\w >'
-
-keep_alive
-
-echo https://dl-cdn.alpinelinux.org/alpine/edge/main > /etc/apk/repositories
-echo https://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories
-
--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@
 cp ./keep_alive /bin/keep_alive
 cp ./profile ~/
 
+echo https://dl-cdn.alpinelinux.org/alpine/v3.12/main > /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositories
