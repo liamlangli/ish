cebcece3e7adaf7919a8a9d4747db9ad39c4d613
lang <lilang8936@gmail.com>
2024-10-13 17:19:15 +0000

    rename profile

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp ./keep_alive /bin/keep_alive
-cp ./profile ~/
+cp ./profile ~/.profile
 
 echo https://dl-cdn.alpinelinux.org/alpine/v3.12/main > /etc/apk/repositories
 echo https://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositories
